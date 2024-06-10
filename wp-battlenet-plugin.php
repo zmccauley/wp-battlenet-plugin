c001e63ae4caf90d023490ef089e703640fafa6f
Zack McCauley <zjmccauley23@gmail.com>
2024-06-10 18:49:39 -0500

    Update wp-battlenet-plugin.php

--- wp-battlenet-plugin.php
+++ wp-battlenet-plugin.php
@@ -12,6 +12,6 @@ add_action('init', function () {
 function say_something(){
     return '<h1>HELLO I AM SAYING SOMETHING</h1>';
 }
-add_shortcode('saySomething', 'say_something');
+add_shortcode('say_something', 'say_something');
 
 ?>
