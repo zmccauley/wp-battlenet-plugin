b0ad680ee998ee2a5c586bc8db85b3ad947949f4
Zack McCauley <zjmccauley23@gmail.com>
2024-06-26 17:08:12 -0500

    Update wp-battlenet-plugin.php

--- wp-battlenet-plugin.php
+++ wp-battlenet-plugin.php
@@ -13,5 +13,18 @@ function say_something_func(){
     return '<h1>HELLO I AM SAYING SOMETHING</h1>';
 }
 add_shortcode('say_something', 'say_something_func');
+/**google hire me bro */
 
+add_action('admin_menu', 'fsdapikey_register_my_api_keys_page');
+
+function fsdapikey_register_my_api_keys_page() {
+  add_submenu_page(
+    'tools.php', // Add our page under the "Tools" menu
+    'API Keys', // Title in menu
+    'API Keys', // Page title
+    'manage_options', // permissions
+    'api-keys', // slug for our page
+    'fsdapikey_add_api_keys_callback' // Callback to render the page
+  );
+}
 ?>
