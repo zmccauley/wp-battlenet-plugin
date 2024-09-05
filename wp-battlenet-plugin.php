07a01864408a35c94f6b10c3804458d871f86aab
Zack McCauley <zjmccauley23@gmail.com>
2024-09-04 22:15:33 -0500

    Update wp-battlenet-plugin.php

--- wp-battlenet-plugin.php
+++ wp-battlenet-plugin.php
@@ -40,12 +40,7 @@ function blizzard_call_func($blizz_id){
   curl_setopt($curl, CURLOPT_HEADER, 'Authorization: Bearer '.$access_token);
   $result = curl_exec($curl);
   curl_close($curl);
-  if($result != ''){
-    return 'nice';
-  } else {
-    return 'not nice';
-  }
-  //return $result;
+  return $result;
 }
 
 add_shortcode('blizzard_call','blizzard_call_func');
