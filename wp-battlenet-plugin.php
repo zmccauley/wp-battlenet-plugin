7bfe86162419ab2aa9241ef071f99704d3de7d45
Zack McCauley <zjmccauley23@gmail.com>
2024-10-03 16:13:19 -0500

    Update wp-battlenet-plugin.php

--- wp-battlenet-plugin.php
+++ wp-battlenet-plugin.php
@@ -106,8 +106,8 @@ function blizzard_api_affixes() {
   curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
   $result = curl_exec($curl);
   curl_close($curl);
-  echo gettype($result);
-  return $result;
+  echo $result;
+  return;
   
 }
     
