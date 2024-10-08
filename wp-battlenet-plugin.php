1e6def594fb55cd9a73dc801ea63fc19f5c72b0e
Zack McCauley <zjmccauley23@gmail.com>
2024-10-08 18:35:46 -0500

    Update wp-battlenet-plugin.php

--- wp-battlenet-plugin.php
+++ wp-battlenet-plugin.php
@@ -168,7 +168,7 @@ function blizzard_api_affixes() {
   $result = json_decode(curl_exec($curl),true);
   curl_close($curl);
 
-return display_affixes($result,$my_creds) . "<div class='hover-text' id='hoverText'></div>" . "
+return "<div class='hover-text' id='hoverText'></div>" . display_affixes($result,$my_creds) .  "
 <style>
         .hover-target {
             display: inline-block;
@@ -199,7 +199,7 @@ return display_affixes($result,$my_creds) . "<div class='hover-text' id='hoverTe
             hoverText.textContent = target.getAttribute('data-text'); // Set the hover text
             hoverText.style.display = 'block'; // Show text on hover
             const rect = target.getBoundingClientRect();
-            hoverText.style.top = `{rect.bottom + window.scrollY}px`; // Position below the hovered element 
+            hoverText.style.top = `${rect.bottom + window.scrollY}px`; // Position below the hovered element 
             hoverText.style.left = `{rect.left}px`; // Align with the left of the hovered element
         });
 
