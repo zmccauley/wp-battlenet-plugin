dbf53636eb98593086b0731996d7f59584b7586a
Zack McCauley <zjmccauley23@gmail.com>
2024-10-10 17:46:46 -0500

    Update affixHover.js

--- js/affixHover.js
+++ js/affixHover.js
@@ -1,9 +1,8 @@
-<script>
-        function updateDateTime() {
-            now = new Date();
-            const dateTimeString = now.toLocaleString();
-            document.getElementById('dateTime').textContent = dateTimeString;
-        
-        updateDateTime();
-        setInterval(updateDateTime, 1000);
-</script>
+
+function updateDateTime() {
+    const now = new Date();
+    const dateTimeString = now.toLocaleString();
+         document.getElementById('dateTime').textContent = dateTimeString;
+        }
+     updateDateTime();
+     setInterval(updateDateTime, 1000);
